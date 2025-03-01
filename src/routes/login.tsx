ecca9b470b1f410d4422a5e589ae9b8114eaa2db
Andres Court <andres@andrescourt.com>
2025-03-01 15:51:54 -0500

    chore(shadcn): installed input and label component

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@/components/ui/input";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
@@ -7,9 +8,16 @@ export const Route = createFileRoute("/login")({
 function RouteComponent() {
     return (
         <div className="flex h-screen justify-center items-center">
-            <h1 className="text-2xl text-primary uppercase tracking-wider font-semibold">
-                Login
-            </h1>
+            <div className="flex flex-col w-full items-center gap-3">
+                <h1 className="text-2xl text-primary uppercase tracking-wider font-semibold">
+                    Login
+                </h1>
+                <div className="w-1/2">
+                    <div>
+                        <Input placeholder="Email" name="email" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
