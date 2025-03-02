204dd89f54f8361d850150adf9276a09d8056046
Andres Court <andres@andrescourt.com>
2025-03-02 17:39:44 -0500

    feat(admin user): display the user's information in a table

--- src/lib/api/users.ts
+++ src/lib/api/users.ts
@@ -19,3 +19,20 @@ export async function meGet() {
 
     return await res.json() as UserResponse;
 }
+
+export async function getAllUsers() {
+    const res = await fetch(`${server}/users`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${loginStore.state.token}`,
+        }
+    });
+
+    if (!res.ok) {
+        const error = await res.json();
+        throw new Error(JSON.stringify(error.error));
+    }
+
+    return await res.json() as UserResponse[];
+}
--- src/routes/_auth/usuarios/administrar.tsx
+++ src/routes/_auth/usuarios/administrar.tsx
@@ -1,9 +1,80 @@
-import { createFileRoute } from '@tanstack/react-router'
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import { getAllUsers } from "@/lib/api/users";
+import type { UserResponse } from "@/types/users";
+import { createFileRoute } from "@tanstack/react-router";
+import {
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+    type ColumnDef,
+} from "@tanstack/react-table";
 
-export const Route = createFileRoute('/_auth/usuarios/administrar')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/_auth/usuarios/administrar")({
+    component: RouteComponent,
+    loader: async ({ context: { queryClient } }) => {
+        return await queryClient.ensureQueryData({
+            queryKey: ["users"],
+            queryFn: getAllUsers,
+        });
+    },
+});
 
 function RouteComponent() {
-  return <div>Hello "/_auth/usuarios/administrar"!</div>
+    const users = Route.useLoaderData();
+    const columns: ColumnDef<UserResponse>[] = [
+        {
+            accessorKey: "name",
+            header: "Nombre",
+        },
+        {
+            accessorKey: "email",
+            header: "Email",
+        },
+    ];
+
+    const table = useReactTable({
+        data: users,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    });
+
+    return (
+        <Table>
+            <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                    <TableRow key={headerGroup.id}>
+                        {headerGroup.headers.map((header) => (
+                            <TableHead key={header.id}>
+                                {header.isPlaceholder
+                                    ? null
+                                    : flexRender(
+                                        header.column.columnDef.header,
+                                        header.getContext(),
+                                    )}
+                            </TableHead>
+                        ))}
+                    </TableRow>
+                ))}
+            </TableHeader>
+
+            <TableBody>
+                {table.getRowModel().rows.map((row) => (
+                    <TableRow key={row.id}>
+                        {row.getVisibleCells().map((cell) => (
+                            <TableCell key={cell.id}>
+                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    );
 }
