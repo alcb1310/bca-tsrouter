547517a5c05d009a8319505d91a01a8f542aed2e
Andres Court <andres@andrescourt.com>
2025-03-02 12:39:08 -0500

    chore: add react query as a context of the router

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,15 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 import TanstackQueryLayout from "@/integrations/tanstack-query/layout";
 
-import TanstackQueryProvider from "@/integrations/tanstack-query/provider";
 import { useStore } from "@tanstack/react-store";
 import { store } from "@/lib/environment-store";
+import type { QueryClient } from "@tanstack/react-query";
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+    queryClient: QueryClient
+}>()({
     component: RootComponent,
 });
 
@@ -16,11 +18,9 @@ function RootComponent() {
 
     return (
         <>
-            <TanstackQueryProvider>
-                <Outlet />
-                {env === "development" && <TanStackRouterDevtools />}
-                {env === "development" && <TanstackQueryLayout />}
-            </TanstackQueryProvider>
+            <Outlet />
+            {env === "development" && <TanStackRouterDevtools />}
+            {env === "development" && <TanstackQueryLayout />}
         </>
     );
 }
